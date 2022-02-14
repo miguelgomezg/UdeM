76de840997e69f3a44c7330a6b0559b9ab1ed911
Miguel Ángel Gómez García <Mgomezg@intergrupo.com>
2022-02-14 15:50:25 -0500

    Avances

--- Algoritmos/TiendaLibros/Models/Tienda.py
+++ Algoritmos/TiendaLibros/Models/Tienda.py
@@ -9,3 +9,9 @@ class Tienda:
     """
     flujo_caja: float
     catalogo: Dict[str, Libro]
+
+    def registrar_libro(self, libro: Libro):
+        if libro.isbn not in self.catalogo.keys():
+            self.catalogo[libro.isbn] = libro
+        else:
+            raise Exception("El libro ya existe en el catalogo")
